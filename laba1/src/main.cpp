0c5075270ecfe9abc618f09054fc81cd2ea09b26
Ilya Bryukhov <ilbryukhov@yandex.ru>
2022-11-28 22:16:05 +0300

    edited main func

--- laba1/src/main.cpp
+++ laba1/src/main.cpp
@@ -121,41 +121,40 @@ void rotate(Rectangle& shape, const sf::Mouse::Button& button) {
   shape.apply(getTranslationMatrix(middlePoint.x, middlePoint.y));
 }
 
+void processEvent(Rectangle& rectangle, const sf::Event& event) {
+  switch (event.type) {
+    case sf::Event::KeyPressed:
+      if (isTranslationKey(event.key.code))
+        translate(rectangle, event.key.code);
+      break;
+
+    case sf::Event::MouseWheelScrolled:
+      scale(rectangle, event.mouseWheelScroll.delta);
+      break;
+
+    case sf::Event::MouseButtonPressed:
+      rotate(rectangle, event.mouseButton.button);
+      break;
+
+    default:
+      break;
+  }
+}
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML works!");
-  Rectangle rectangel({200, 200}, 100, 50);
-  rectangel.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Laba1");
+  Rectangle rectangle({200, 200}, 100, 50);
+  rectangle.setFillColor(sf::Color::Green);
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
-    switch (event.type) {
-      // window closed
-      case sf::Event::Closed:
-        window.close();
-        break;
-      // key pressed
-      case sf::Event::KeyPressed:
-        if (isTranslationKey(event.key.code))
-          translate(rectangel, event.key.code);
-        break;
-
-      case sf::Event::MouseWheelScrolled:
-        scale(rectangel, event.mouseWheelScroll.delta);
-        break;
-
-      case sf::Event::MouseButtonPressed:
-        rotate(rectangel, event.mouseButton.button);
-        break;
-      // we don't process other types of events
-      default:
-        break;
-    }
+    processEvent(rectangle, event);
 
     window.clear();
-    window.draw(rectangel.getShape());
+    window.draw(rectangle.getShape());
     window.display();
   }
 
